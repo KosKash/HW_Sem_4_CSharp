216f1108f6ebbe5d9ecf3f6e8a9c4a8c53d4fe73
Constantine <koskash98ksband@gmail.com>
2022-07-07 18:21:34 +0600

    Ex2 Done

---
+++ Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+Console.Clear();
+System.Console.Write("Введите число ");
+string num = Console.ReadLine();
+int[] ms = new int[num.Length];
+for (int i = 0; i < num.Length; i++)
+{
+    ms[i] = int.Parse(num[i].ToString());
+}
+int sum = ms.Sum();
+System.Console.WriteLine(sum);
+
